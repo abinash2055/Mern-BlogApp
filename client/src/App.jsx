47aaafaf423a9a0679673dafc8f689868afcccf1
abinash2055 <abinashnathpandey@gmail.com>
2026-04-14 11:04:22 +0545

    shadcn ui violet done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import { Button } from './components/ui/button';
 
 const App = () => {
   return (
     <div>
-      <h1 class="text-3xl font-bold underline">Hello world!</h1>
+      <h1 class="text-3xl font-bold underline">
+        Hello world!
+        <Button>Shadcn UI Button </Button>
+      </h1>
     </div>
   );
 };
