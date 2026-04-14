a76fc8477dff52ee2e0322f7199eb8d5b93e4843
abinash2055 <abinashnathpandey@gmail.com>
2026-04-14 13:56:42 +0545

    Client signin signup

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,7 +1,105 @@
+import { Button } from '@/components/ui/button';
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
 import React from 'react';
+import { z } from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
+import { Card } from '@/components/ui/card';
+import { Link } from 'react-router-dom';
+import { RouteSignUp } from '@/helpers/RouteName';
 
 const SignIn = () => {
-  return <div>SignIn</div>;
+  const formSchema = z.object({
+    email: z.string().email(),
+    password: z.string().min(8, {
+      message: 'Password must be at least 8 characters long.',
+    }),
+  });
+
+  const form = useForm({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: '',
+      password: '',
+    },
+  });
+
+  function onSubmit(values) {
+    console.log(values);
+  }
+
+  return (
+    <div className="flex justify-center items-center h-screen w-screen">
+      <Card className="w-[400px] p-4">
+        <h1 className="text-2xl font-bold text-center mb-5">
+          Login Into Account
+        </h1>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)}>
+            {/* Email */}
+            <div className="mb-3">
+              <FormField
+                control={form.control}
+                name="email"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Email</FormLabel>
+                    <FormControl>
+                      <Input
+                        placeholder="Enter your email address"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+
+            {/* Password */}
+            <div className="mb-3">
+              <FormField
+                control={form.control}
+                name="password"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Password</FormLabel>
+                    <FormControl>
+                      <Input placeholder="Enter your password" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+
+            <div className="mt-5">
+              <Button type="submit" className="w-full">
+                Sign In
+              </Button>
+              <div className="mt-5 text-sm flex justify-center items-center gap-2">
+                <p>Don&apos;t have an account?</p>
+                <Link
+                  className="text-blue-500 hover:underline"
+                  to={RouteSignUp}
+                >
+                  Sign Up
+                </Link>
+              </div>
+            </div>
+          </form>
+        </Form>
+      </Card>
+    </div>
+  );
 };
 
 export default SignIn;
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,7 +1,153 @@
+import { Button } from '@/components/ui/button';
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
 import React from 'react';
+import { z } from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
+import { Card } from '@/components/ui/card';
+import { Link } from 'react-router-dom';
+import { RouteSignIn } from '@/helpers/RouteName';
 
 const SignUp = () => {
-  return <div>SignUp</div>;
+  const formSchema = z
+    .object({
+      name: z.string().min(3, {
+        message: 'Name must be at least 3 characters long.',
+      }),
+      email: z.string().email(),
+      password: z.string().min(8, {
+        message: 'Password must be at least 8 characters long.',
+      }),
+      confirmPassword: z.string(),
+    })
+    .refine((data) => data.password === data.confirmPassword, {
+      message: 'Passwords do not match with Confirm Password.',
+      path: ['confirmPassword'],
+    });
+
+  const form = useForm({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: '',
+      email: '',
+      password: '',
+      confirmPassword: '',
+    },
+  });
+
+  function onSubmit(values) {
+    console.log(values);
+  }
+
+  return (
+    <div className="flex justify-center items-center h-screen w-screen">
+      <Card className="w-[400px] p-4">
+        <h1 className="text-2xl font-bold text-center mb-5">
+          Create Your Account
+        </h1>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)}>
+            {/* Name */}
+            <div className="mb-3">
+              <FormField
+                control={form.control}
+                name="name"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Name</FormLabel>
+                    <FormControl>
+                      <Input placeholder="Enter your name" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+
+            {/* Email */}
+            <div className="mb-3">
+              <FormField
+                control={form.control}
+                name="email"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Email</FormLabel>
+                    <FormControl>
+                      <Input
+                        placeholder="Enter your email address"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+
+            {/* Password */}
+            <div className="mb-3">
+              <FormField
+                control={form.control}
+                name="password"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Password</FormLabel>
+                    <FormControl>
+                      <Input placeholder="Enter your password" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+
+            {/* Confirm Password */}
+            <div className="mb-3">
+              <FormField
+                control={form.control}
+                name="confirmPassword"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Confirm Password</FormLabel>
+                    <FormControl>
+                      <Input
+                        placeholder="Enter your password again"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+
+            <div className="mt-5">
+              <Button type="submit" className="w-full">
+                Sign Up
+              </Button>
+              <div className="mt-5 text-sm flex justify-center items-center gap-2">
+                <p>Already have an account?</p>
+                <Link
+                  className="text-blue-500 hover:underline"
+                  to={RouteSignIn}
+                >
+                  Sign In
+                </Link>
+              </div>
+            </div>
+          </form>
+        </Form>
+      </Card>
+    </div>
+  );
 };
 
 export default SignUp;
